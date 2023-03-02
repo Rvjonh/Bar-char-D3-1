de3a159d138529624f978643b8ac1d041916c2ed
Rvjonh <jonhvelasco3@gmail.com>
2023-03-01 23:28:47 -0400

    v1.0.1

--- code.js
+++ code.js
@@ -19,20 +19,32 @@ function drawBarChar(dataset) {
 
     const svg = d3.select("#graph").append("svg").attr("width", W).attr("height", H)
 
-    /* X axis */
-    var yearsDate = dataset.map(function (item) {
-        new_date = new Date(item[0])
-        console.log(new_date)
-        return new_date;
-    });
-    var xMax = new Date(d3.max(yearsDate));
-    //xMax.setMonth(xMax.getMonth() + 3);
+    const tooltip = d3.select("#graph")
+        .append("div")
+        .attr('id', 'tooltip')
+        .style('height', '80px')
+        .style('width', '140px')
+        .attr("class", "car-tooltip")
+        .style('position', 'absolute')
+        .style('opacity', 0)
+        .style('font-size', '18px')
 
-    var xScale = d3.scaleTime()
-        .domain([d3.min(yearsDate), xMax])
+    const toolTime = d3.select("#tooltip").append("time")
+    const toolMoney = d3.select("#tooltip").append("span")
+
+
+    /* X axis */
+    const xPositionScale = d3.scaleTime()
+        .domain([
+            d3.min(dataset, (d) => new Date(d[0])),
+            d3.max(dataset, (d) => new Date(d[0])),
+        ])
         .range([P, W - P]);
 
-    var xAxis = d3.axisBottom(xScale)
+    const xAxis = d3
+        .axisBottom(xPositionScale)
+        .tickFormat((val) => new Date(val).getFullYear())
+        .ticks(W / 60);
 
     svg.append('g')
         .call(xAxis)
@@ -70,9 +82,7 @@ function drawBarChar(dataset) {
             return dataset[i][1];
         })
         .attr('class', 'bar')
-        .attr('x', function (d, i) {
-            return xScale(yearsDate[i]);
-        })
+        .attr("x", (d, i) => xPositionScale(new Date(dataset[i][0])))
         .attr('y', function (d) {
             return H - d - P;
         })
@@ -81,23 +91,32 @@ function drawBarChar(dataset) {
             return d;
         })
         .attr('index', (d, i) => i)
-        .style('fill', '#33adff')
-        .on('mouseover', function (event, d) {
+        .attr('fill', '#33adff')
+        .style("cursor", "pointer")
+        .on('mouseover', function (e, d) {
             let i = this.getAttribute('index');
 
-            svg.append("rect")
-                .attr("id", "tooltip")
-                .attr("x", 50)
-                .attr("y", 50)
-                .attr("width", 100)
-                .attr("height", 100)
-                .attr("fill", "red")
+            const h = i / dataset.length
+            e.target.style.fill = "rgba(11, 96, 12," + h + ")";
+
+            tooltip.style("opacity", "1")
                 .attr("data-date", () => {
                     return dataset[i][0];
                 })
+                .style("top", (e.clientY - 85) + "px")
+                .style("left", d => {
+                    new_x = e.clientX;
+                    if (new_x > W / 2) {
+                        new_x -= 140
+                    }
+                    return new_x + "px"
+                })
+            toolTime.text(dataset[i][0])
+            toolMoney.text(dataset[i][1] + "$ Billion")
         })
         .on('mouseout', function (event, d) {
-            const tool = d3.select("#tooltip").remove()
+            event.target.style.fill = "#33adff";
+            tooltip.style("opacity", "0")
         })
 
 }
